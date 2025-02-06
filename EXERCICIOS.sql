54e77c0f8ea22c8d150a52ba539eef3c4d3970a5
Jose Diego Mendes da silva <122491768+Diegomendessilva@users.noreply.github.com>
2025-02-06 19:41:59 -0300

    Update EXERCICIOS.sql

--- EXERCICIOS.sql
+++ EXERCICIOS.sql
@@ -1,7 +1,7 @@
---VocÍ È o gerente da ·rea de compras e precisa criar um relatÛrio com as
---TOP 100 vendas, de acordo com a quantidade vendida. VocÍ precisa fazer
---isso em 10min pois o diretor de compras solicitou essa informaÁ„o para
---apresentar em uma reuni„o.
+--Voc√™ √© o gerente da √°rea de compras e precisa criar um relat√≥rio com as
+--TOP 100 vendas, de acordo com a quantidade vendida. Voc√™ precisa fazer
+--isso em 10min pois o diretor de compras solicitou essa informa√ß√£o para
+--apresentar em uma reuni√£o.
 --Utilize seu conhecimento em SQL para buscar essas TOP 100 vendas, de
 --acordo com o total vendido (SalesAmount).
 
@@ -12,10 +12,10 @@ from FactSales
 
 --ex 2
 --Os TOP 10 produtos com maior UnitPrice possuem exatamente o mesmo
---preÁo. PorÈm, a empresa quer diferenciar esses preÁos de acordo com o
+--pre√ßo. Por√©m, a empresa quer diferenciar esses pre√ßos de acordo com o
 --peso (Weight) de cada um.
---O que vocÍ precisar· fazer È ordenar esses top 10 produtos, de acordo
---com a coluna de UnitPrice e, alÈm disso, estabelecer um critÈrio de
+--O que voc√™ precisar√° fazer √© ordenar esses top 10 produtos, de acordo
+--com a coluna de UnitPrice e, al√©m disso, estabelecer um crit√©rio de
 --desempate, para que seja mostrado na ordem, do maior para o menor.
 
 
@@ -24,7 +24,7 @@ from DimProduct
 order by UnitPrice
 
 --Caso ainda assim haja um empate entre 2 ou mais produtos, pense em
---uma forma de criar um segundo critÈrio de desempate (alÈm do peso).
+--uma forma de criar um segundo crit√©rio de desempate (al√©m do peso).
 select top (10)
 
 select top (10) UnitPrice, weight
@@ -32,14 +32,14 @@ from DimProduct
 order by UnitPrice desc, Weight desc
 
 --ex 3 
---VocÍ È respons·vel pelo setor de logÌstica da empresa Contoso e precisa
+--Voc√™ √© respons√°vel pelo setor de log√≠stica da empresa Contoso e precisa
 --dimensionar o transporte de todos os produtos em categorias, de acordo
 --com o peso.
---Os produtos da categoria A, com peso acima de 100kg, dever„o ser
+--Os produtos da categoria A, com peso acima de 100kg, dever√£o ser
 --transportados na primeira leva.
---FaÁa uma consulta no banco de dados para descobrir quais s„o estes
---produtos que est„o na categoria A.
---a. VocÍ dever· retornar apenas 2 colunas nessa consulta: Nome do
+--Fa√ßa uma consulta no banco de dados para descobrir quais s√£o estes
+--produtos que est√£o na categoria A.
+--a. Voc√™ dever√° retornar apenas 2 colunas nessa consulta: Nome do
 --Produto e Peso.
 
 select ProductName, Weight from DimProduct
@@ -56,10 +56,10 @@ from DimProduct
 order by nome desc, peso desc
 
 
---4 VocÍ foi alocado para criar um relatÛrio das lojas registradas atualmente
+--4 Voc√™ foi alocado para criar um relat√≥rio das lojas registradas atualmente
 --na Contoso.
---vocÍ dever· fazer ‡ tabela DimStore, retorne as seguintes
---informaÁıes: StoreName, OpenDate, EmployeeCount.
+--voc√™ dever√° fazer √† tabela DimStore, retorne as seguintes
+--informa√ß√µes: StoreName, OpenDate, EmployeeCount.
 
 select StoreName,OpenDate,EmployeeCount
 from DimStore
@@ -70,7 +70,7 @@ from DimStore
 select StoreName as 'nome da loja',OpenDate as 'data',EmployeeCount as 'contagem de funcionario'
 from DimStore
 
---c. Dessas lojas, descubra quantas (e quais)lojas ainda est„o ativas.
+--c. Dessas lojas, descubra quantas (e quais)lojas ainda est√£o ativas.
 select Status
 from DimStore
 where status like 'on'
@@ -80,13 +80,13 @@ from DimStore
 where StoreType = 'store' OR status = 'ON'
 
 
---ex 5 O gerente da ·rea de controle de qualidade notificou ‡ Contoso que
+--ex 5 O gerente da √°rea de controle de qualidade notificou √† Contoso que
 --todos os produtos Home Theater da marca Litware, disponibilizados
---para venda no dia 15 de marÁo de 2009, foram identificados com
---defeitos de f·brica.
---O que vocÍ dever· fazer È identificar os IDís desses produtos e repassar
+--para venda no dia 15 de mar√ßo de 2009, foram identificados com
+--defeitos de f√°brica.
+--O que voc√™ dever√° fazer √© identificar os ID‚Äôs desses produtos e repassar
 --ao gerente para que ele possa notificar as lojas e consequentemente
---solicitar a suspens„o das vendas desses produtos.
+--solicitar a suspens√£o das vendas desses produtos.
 
 select 
 	ProductKey as 'id produto',
@@ -102,33 +102,33 @@ where
 
 
 
---6 Imagine que vocÍ precise extrair um relatÛrio da tabela DimStore, com
---informaÁıes de lojas. Mas vocÍ precisa apenas das lojas que n„o est„o
+--6 Imagine que voc√™ precise extrair um relat√≥rio da tabela DimStore, com
+--informa√ß√µes de lojas. Mas voc√™ precisa apenas das lojas que n√£o est√£o
 --mais funcionando atualmente.
 --a. Utilize a coluna de Status para filtrar a tabela e trazer apenas as
---lojas que n„o est„o mais funcionando.
+--lojas que n√£o est√£o mais funcionando.
 
 select Status
 from DimStore
 where Status like 'off'
 
---b Agora imagine que essa coluna de Status n„o existe na sua tabela.
---Qual seria a outra forma que vocÍ teria de descobrir quais s„o as lojas
---que n„o est„o mais funcionando.
+--b Agora imagine que essa coluna de Status n√£o existe na sua tabela.
+--Qual seria a outra forma que voc√™ teria de descobrir quais s√£o as lojas
+--que n√£o est√£o mais funcionando.
 
 select CloseReason
 from DimStore
 where CloseReason like 'relocation'
 
---ex 7 determinada quantidade de m·quinas de cafÈ. As lojas ser„o divididas em
+--ex 7 determinada quantidade de m√°quinas de caf√©. As lojas ser√£o divididas em
 --3 categorias:
 
---CATEGORIA 1:De 1 a 20 funcion·rios ->1 m·quina de cafÈ
---CATEGORDe acordo com a quantidade de funcion·rios, cada loja receber· uma
---IA 2:De 21 a 50 funcion·rios ->2 m·quinas de cafÈ
---CATEGORIA 3: Acima de 51 funcion·rios ->3 m·quinas de cafÈ
---Identifique, para cada caso, quais s„o as lojas de cada uma das 3
---categorias acima (basta fazer uma verificaÁ„o).
+--CATEGORIA 1:De 1 a 20 funcion√°rios ->1 m√°quina de caf√©
+--CATEGORDe acordo com a quantidade de funcion√°rios, cada loja receber√° uma
+--IA 2:De 21 a 50 funcion√°rios ->2 m√°quinas de caf√©
+--CATEGORIA 3: Acima de 51 funcion√°rios ->3 m√°quinas de caf√©
+--Identifique, para cada caso, quais s√£o as lojas de cada uma das 3
+--categorias acima (basta fazer uma verifica√ß√£o).
 
 
 SELECT * 
@@ -154,9 +154,9 @@ WHERE EmployeeCount >= 51
 
 
 
---ex 8 A empresa decidiu que todos os produtos LCD receber„o um super
---desconto no prÛximo mÍs. O seu trabalho È fazer uma consulta ‡ tabela
---DimProduct e retornar os IDís, Nomes e PreÁos de todos os produtos
+--ex 8 A empresa decidiu que todos os produtos LCD receber√£o um super
+--desconto no pr√≥ximo m√™s. O seu trabalho √© fazer uma consulta √† tabela
+--DimProduct e retornar os ID‚Äôs, Nomes e Pre√ßos de todos os produtos
 --LCD existentes.
 
 select ClassID,
@@ -177,8 +177,33 @@ where ProductDescription like '%LCD%'
 
 select * from DimProduct
 
+--ex 9 Fa√ßa uma lista com todos os produtos das cores: Green, Orange, Black,
+--Silver e Pink. Estes produtos devem ser exclusivamente das marcas:
+--Contoso,Litware e Fabrikam.
 
+select 
+	BrandName,
+	ColorName
+from DimProduct
+where 
+	BrandName in  ('Contoso','Litware','Fabrikam')
+	AND ColorName in(  'Green','Orange','Black',
+	'Silver', 'Pink')
 
+-- ex 10 A empresa possui 16 produtos da marca Contoso, da cor Silver e com um
+--UnitPrice entre 10 e 30. Descubra quais s√£o esses produtos e ordene
+--o resultado em ordem decrescente de acordo com o pre√ßo (UnitPrice).
+
+select Weight,
+	BrandName,
+	ColorName
+from DimProduct
+where BrandName = 'Contoso'
+and ColorName = 'Silver'
+and Weight  between 10 and 30
+order by Weight desc 
+
+-- exemplos	
 SELECT * FROM DimProduct
 WHERE NOT ColorName = 'BLUE'
 
